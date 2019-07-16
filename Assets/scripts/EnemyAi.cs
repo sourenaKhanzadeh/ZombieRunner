d401079cb9b8cf54d0446c0d2e7b9b43917af9d8
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-07-15 21:24:33 -0400

    Enemy Agent Created

---
+++ Assets/scripts/EnemyAi.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+    float distanceTarget = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTarget = Vector3.Distance(target.position, transform.position);
+
+        if(distanceTarget <= chaseRange)
+            navMeshAgent.SetDestination(target.position);
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
